74e01ac360d1ca20b13425cb48d752bef83044f5
hyeonmin11 <79415795+hyeonmin11@users.noreply.github.com>
2023-06-25 22:23:39 +0900

    1-1 주석까지 완료 & 복습전

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -60,7 +60,77 @@ public class PlayerController : MonoBehaviour
         myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY)); // 오일러각 쿼터니언으로 변경시켜 사용한다.
 
     }
-
-
-
 }
+
+/*
+캐릭터의 크기는 캡슐의 크기로 어림 잡으면 편하다.
+Main Camera 오브젝트를 A라는 이름의 오브젝트의 자식으로 넣으면 Main Camera는 A의 1인칭 카메라가 된다. 즉 A의 시점으로 화면을 비춘다. 카메라의 위치가 A의 시점이 되도록 Transform값만
+조정해주면 A를 따라다니며 A의 시점을 촬용하는 1인칭 카메라가 된다.
+
+
+유니티에서는 X축 이동 -> 좌우, Z축 이동 -> 앞뒤, Y축 이동 -> 위아래
+myRigid : Rigidbody컴포넌트를 할당할 변수로 private하기 때문에 게임 시작시 GetComponent 함수를 통해 할당해준다.
+_moveDirX : 좌우 이동 크기  A: -1 D : 1 (스칼라)
+_moveDirZ : 앞뒤 이동 크기 S : -1 W : 1
+
+_moveHorizontal : 좌우 이동 벡터값
+방향 * 크기 = transform.right * _moveDirX (transform.right은 오브젝트의 오른쪽 방향을 나타내는 방향단위벡터)
+
+_moveVertical : 앞뒤 이동 벡터값
+
+(현재의 위치 + 속도 X 델타타임)
+Move() 함수는 업데이트함수 내부에서 실행될 것이기 때문에 1초동안 _velocity이 더해질 수 있도록 1프레임 당 1/60 * _velocity만큼 더해주어야 한다.
+Time.deltaTime이 1/60이 된다.
+*/
+
+/*
+해당 축을 고정한 체 해당 축의 입장에서 시계 방향으로 회전한다. 따라서 X축 회전은 고개를 숙이는게 Positive 방향 회전이다.
+X축 회전: X축을 중심으로 회전한다는 의미. X축 회전값은 변함 없다.
+X축은 평면가로축(좌우)
+따라서 마우스를 위아래로 움직이면 고개를숙였다 올렸다 하는 1인칭 시점을 구현해야 하므로 오브젝트의 머리에 달려있는 1인칭 카메라인 MainCamera를 X축 회전해주어야 한ㄷ아.
+오브젝트는 가만히 있는 상태에서 카메라만 X축으로 회전시키는 것이다.
+
+
+Y축 회전: 
+Y축은 수직축
+마우스를 양옆으로 움직이면 고개를 좌우로 돌리는 1인칭 시점을 구현해야 할 때 y축 회전시킨다.
+카메라는 가만히 있고 오브젝트는 Y축으롤 회전. 몸을 마우스가 향하는 방향으로 돌린다.
+
+Z축 회전:
+Z축은 평면 수직축
+Z축은 양 옆으로 굴러가는 회전을 하는 것과 같다.
+*/
+
+/*
+theCamera는 메인 카메라가 할당된다. 컴포넌트가 아니라서 GetComponent로 가져올 수 없고 private이지만 유니티 에디터에서 할당해줄 수 있도록
+SieralizeFiled 속성을 붙여주었다. Hierarchy 창에서 해당하는 오브젝트를 찾아주는 FindObjectOfType함수를 사용할 수도 있지만 오브젝트가 굉장히 많다면
+너무 느리고 카메라도 여러개를 둘 수 있기 때문에 이 방법은 비추한다.
+
+마우스 커서는 2d다. x y 축 이동 밖에 없다.
+Input.GetAxisRaw("Mouse Y")역시 -1 0 1 만 나타낸다.
+마우스가 아래로 움직이면 -1 위로 움직이면 1 , 안 움직이면 0
+
+카메라가 X축으로 회전할 만큼의 양 : _cameraRotationX
+ : 마우스가 Y 방향으로 움직인 정도 * 민감도
+
+카메라 x축 방향 회전 값 currentCameraRotationX
+ : _cameraRotationX 만큼 뺴준다.
+ x축 방향으로 회전할 때 아래로 숙이는게 Positive 방향이고 정면을 보는게 0이고 위로 젖히는게 Negative 방향이다.
+ 마우스가 아래로 움직이면 _cameraRotationX값이 Input.GetAxisRaw에 의해 음수가 되기 때문에 
+ _cameraRoationX만큼을 더해주게 되면 currentCameraRotationX값이 감소하여 카메라가 위로 회전하게 된다.
+ 따라서 += 로 해주면 오히려 마우스를 아래로 내릴 수록 위를 쳐다보게 되기 때문에
+ 마우스가 아래로 움직이면 카메라의 x축회전값이 커져 카메라가 아래로 숙이게끔 -= QOwndjTek.
+
+ Mathf.Clamp를 사용하여 최대최소 회전범위 설정했다. ( 마우스를 위아래로 움직일 때 360도 회전해버리면 안되고 고개를 젖힐 때나 숙일때나 어느정도 제한을 두어야해서)
+
+ 두 쿼터니언의 회전량을 더하려면 두 쿼터니언 값을 곱해야한다.
+
+ Rigidbody의 경우 캡슐오브젝트가 밑이 동그래서 자꾸 양옆 앞뒤로 넘어지므로 X Z회전은 안되게끔 막아두었다.
+*/
+
+/*
+
+ 키보드입력에 따른 이동
+ 마우스 Y회전에 따른 카메라의 X축 회전
+ 마우스 X회전에 따른 캡슐의 Y축 회전
+*/
